131ffa5ab7bf086184144508915828a7a514aa23
Jonathon Suggs <jsuggs@murmp.com>
2012-08-26 16:07:06 -0500

    Saving of images

--- src/app.php
+++ src/app.php
@@ -2,12 +2,15 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 
-$debug = false;
+$app['debug'] = false; //in_array($app['request']->server->get('REMOTE_ADDR'), array('127.0.0.1', '::1'));
 
 $twigOptions = array();
-if ($debug) {
+if ($app['debug']) {
     $twigOptions['cache'] = __DIR__ . '/../cache';
 }
 
@@ -16,12 +19,42 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.options' => $twigOptions,
 ));
 
+$app->match('/create', function () use ($app) {
+    return $app['twig']->render('create.html.twig');
+});
+
+$app->post('/save', function (Request $request) {
+    // Get the data
+    $imageData = $GLOBALS['HTTP_RAW_POST_DATA'];
+    $filteredData = substr($imageData, strpos($imageData, ",") + 1);
+    $unencodedData = base64_decode($filteredData);
+
+    // Generate a unique ID (
+    $id = new \MongoId();
+    $filename = sprintf('%s/%s.png', __DIR__ . '/../uploads', $id);
+
+    $fh = fopen($filename, 'w+b');
+    fwrite($fh, $unencodedData);
+    fclose($fh);
+
+    $data = array(
+        'success' => true,
+        'id' => $id
+    );
+
+    return new Response(json_encode($data), 200, array(
+        'Content-type' => 'application/json'
+    ));
+});
+
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig');
 });
 
 $app->error(function (\Exception $e, $code) {
-    var_dump($e);
+    if (in_array($app['request']->server->get('REMOTE_ADDR'), array('127.0.0.1', '::1'))) {
+        var_dump($e);
+    }
     return new Response('We are sorry, but something went terribly wrong.', $code);
 });
 
